19a1d5bf430fdc9c45b59f80465c12a453a3abaa
Maanya Shukla <maanyashukla001@gmail.com>
2026-06-16 22:55:32 -0400

    final changes made to app. README updated

---
+++ components/Flashcard.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Flashcard = (props) => {
+    const [flipped, setFlipped] = useState(false);
+    const updateFlipped= () => {
+        setFlipped(!flipped)
+    }
+    return(
+        <div className={`FlashcardText ${props.color}`} onClick={updateFlipped}>
+            <h3>{flipped ? props.description : props.title}</h3>
+        </div>
+    )
+}
+export default Flashcard;
--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { use, useState } from 'react'
 import './App.css'
+import Flashcard from '../components/Flashcard'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+const App = () => {
+const array = [
+  {title: "안녕하세요 (Annyeonghaseyo)", description: "Hello", color:"green"}, 
+  {title: "감사합니다 (Ghamsahamnida)", description: "Thank you", color:"green"}, 
+  {title: "죄송합니다 (Jwisonghamnida)", description: "Sorry", color:"yellow"}, 
+  {title: "네 (Ne)", description: "Yes", color:"green"}, 
+  {title: "아니요 (Aniyo)", description: "No", color:"green"}, 
+  {title: "저기요 (Jeogiyo)", description: "Excuse me", color:"yellow"}, 
+  {title: "얼마예요? (Eolmayeyo?)", description: "How much?", color:"red"}, 
+  {title: "주세요 (Juseyo)", description: "Please give me.", color:"red"}, 
+  {title: "화장실이 어디예요? (Hwajangsiri eodiyeyo?)", description: "Where is the restroom?", color:"red"}, 
+  {title: "사람 (Saram)", description: "Person", color:"yellow"}, 
+  {title: "대박 (Daebak)", description: "Awesome", color:"yellow"}];
+const [count, setCount] = useState(0);
+const incrementCount = () => {
+  setCount(Math.floor(Math.random()*(array.length)));
 }
 
-export default App
+
+/*const [descr, setDescr] = useState({array[count][title]})
+const updateDescr = () => {
+  s
+}*/
+
+  return(
+    <div className='App'>
+      <div className='header'>
+        <h1>The Ultimate Korean Guide!</h1>
+        <h2>How good are you at the Korean basics? Test all your Korean knowledge here!</h2>
+        <h3>Number of cards: {array.length}</h3>
+      </div>
+      <div className='Flashcard'>
+        <Flashcard title={array[count].title} description={array[count].description} color={array[count].color}/>
+      </div>
+      <div className='buttons'>
+        <button onClick={incrementCount}>Next</button>
+      </div>
+    </div>
+  )
+
+} 
+export default App;
